07147fc527f11ea98adc0b5548f4892f1bfee422
NeoflasH <42207707+NeoflasH@users.noreply.github.com>
2018-08-09 17:43:23 +0200

    Update index.js

--- index.js
+++ index.js
@@ -15,8 +15,9 @@ bot.on('message', message => {
     if (message.content === prefix + "help"){
         message.channel.sendMessage("**__Aide:__** \n -__help:__ afficher ce message \n -__youtube:__ afficher la chaîne youtube de NeoflasH");
     }
+});
 bot.on('message', message => {
     if (message.content === prefix + "youtube"){
         message.channel.sendMessage("Chaîne Youtube de NeoflasH : https://www.youtube.com/channel/UCDBIzgcBjtlGjnj6jkbZsnA?");
     }
-})
+});
